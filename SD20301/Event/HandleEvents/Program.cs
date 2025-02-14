cbc9aa7009a28bbd844f2e62b40bef0c292ee2f0
hoangnhut866 <107618593+hoangnhut866@users.noreply.github.com>
2025-02-14 09:20:30 +0000

    Implement event handling in HandleEvents project and add new projects for null-conditional and inheritance examples

--- SD20301/Event/HandleEvents/Program.cs
+++ SD20301/Event/HandleEvents/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public delegate void MyEventHandler(object sender, EventArgs e);
+
+class MyClass
+{
+    //Declare the event
+    public event MyEventHandler? MyEvent;
+    protected virtual void OnMyEvent()
+    {
+        MyEvent?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void DoSomeThing(){
+        //Some logic here
+        OnMyEvent(); //Raise the event
+    }
+
+}
+
+class Subscriber
+{
+    public void HandleEvent(object sender, EventArgs e)
+    {
+        Console.WriteLine("Event occurred!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myObject = new MyClass();
+        Subscriber subscriber = new Subscriber();
+
+        //Subscribe to the event by attaching a method to it
+        myObject.MyEvent += subscriber.HandleEvent;
+        myObject.DoSomeThing();
+    }
+} 
---
+++ SD20301/Null-conditionalOperator/null-conditional/Program.cs
@@ -0,0 +1,11 @@
+﻿class People
+{
+    public string Name{get;set;}
+
+}
+
+class Program
+{
+    People people = new People();
+    
+}
