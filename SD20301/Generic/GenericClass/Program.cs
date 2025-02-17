1a294500392e721f6e01da2a32784aa896ddc21c
hoangnhut866 <107618593+hoangnhut866@users.noreply.github.com>
2025-02-17 09:30:39 +0000

    Add GenericClass and GenericMethod projects with implementations for generic list and utility methods

---
+++ SD20301/Generic/GenericClass/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+
+class GenericList<T>
+{
+    List<T> list = new List<T>();
+
+    public void Add(T item)
+    {
+        list.Add(item);
+    }
+
+    public T Get(int index)
+    {
+        return list[index];
+    }
+
+   /*  public int Count()
+    {
+        return list.Count;
+    } */
+
+    public int Count{
+        get{
+            return list.Count;
+        }
+    }
+
+    public void PrintAllElement(){
+        Console.WriteLine("All elements of the list:");
+        foreach (var item in list)
+        {
+            Console.WriteLine($"{item}\t");
+        }
+    }
+}
+
+class Program
+{   
+    static void Main(string[] args)
+    {
+        //Using the generic list with intergers
+        GenericList<int> intList = new GenericList<int>();
+        intList.Add(1);
+        intList.Add(2);
+        intList.Add(3);
+        intList.PrintAllElement();
+
+        //Using the genicric list with strings
+        GenericList<string> stringList = new GenericList<string>();
+        stringList.Add("Hello");
+        stringList.Add("Hi");
+        stringList.Add("Nhut");
+        stringList.PrintAllElement();
+    }
+}
---
+++ SD20301/Generic/GenericMethod/GenericMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿class Utilities
+{
+    //Generic method to swap two values
+    public void Swap<T>(ref T a, ref T b)
+    {
+        (a, b) = (b, a);
+    }
+
+    //Generic method with multiple type parameters
+    public void PrintPair<T1, T2>(T1 first, T2 second)
+    {
+        Console.WriteLine($"First: {first}, Second: {second}");
+        Console.WriteLine(first?.GetType());
+        Console.WriteLine(second?.GetType());
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Utilities utilities = new Utilities();
+
+        //Swap intergers
+        int x = 3;
+        int y = 5;
+        utilities.Swap<int>(ref x, ref y);
+        Console.WriteLine($"After swap: x = {x}, y = {y}");
+
+        //Swap strings
+        string a = "Xin chao";
+        string b = "Hello";
+        utilities.Swap<string>(ref a, ref b);
+        Console.WriteLine($"After swap: a = {a}, b = {b}");
+
+        //Print a pair of int and string
+        utilities.PrintPair<int, string>(1, "Nhut");
+
+        //Print a pair of double and boolean
+        utilities.PrintPair<double, bool>(3.45, false);
+
+    }
+}
